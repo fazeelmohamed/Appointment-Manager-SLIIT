aa8b399eb75d51472d4176c08e7f7e8450ee7cb8
fazeelmohamed <mffazeelmohamed2@gmail.com>
2016-03-28 11:30:09 +0530

    Admin's classes

--- Appointment-Manager-SLIIT/app/src/main/java/lk/sliit/appointment/appointment_manager_sliit/AdminDashboard.java
+++ Appointment-Manager-SLIIT/app/src/main/java/lk/sliit/appointment/appointment_manager_sliit/AdminDashboard.java
@@ -10,6 +10,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -21,6 +22,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -46,8 +48,9 @@ public class AdminDashboard extends AppCompatActivity {
 
     CheckBox ckbSPDC,ckbSE3,ckbHCI,ckbSEP,ckbMAD,ckbSE2,ckbITP,ckbITA,ckbPS,ckbSPD,
             ckbST2,ckbST1;
-    Button btnNext1;
-
+    Button btnNext1,btnLogout;
+    String subjectStr = "";
+    JSONObject jsonObject;
     private RequestQueue requestQueue;
     private static final String dashboard_url = "http://quick-appointment.b2creations.net/admin_dashboard.php";
     private StringRequest request;
@@ -74,196 +77,131 @@ public class AdminDashboard extends AppCompatActivity {
         ckbST1 = (CheckBox)findViewById(R.id.ckbST1);
 
         btnNext1 = (Button)findViewById(R.id.btnNext1);
-
-
-
-
-
-
-
-        //Returns the enabled status for this view.
-        //dualcamera1.isEnabled()  //return True if this view is enabled, false otherwise.
-
-        //Indicates whether the view is currently in pressed state.
-        //dualcamera1.isPressed() //return True if this view is enabled, false otherwise.
-
+        btnLogout = (Button)findViewById(R.id.btnLogout);
 
     }
 
-    public void callAdminScheduleActivity(){
+    public void callAdminScheduleActivity(String lid,String[] dayArray){
         Intent intent = new Intent(this, AdminSchedule.class);
+        intent.putExtra("ID",lid);
+        intent.putExtra("dayArray",dayArray);
         startActivity(intent);
     }
+
+    public void btnLogoutClick(View view){
+        //monday = "mon";
+        Intent intent = new Intent(this,Login.class);
+        this.startActivity(intent);
+    }
+
+
     public void btnNext1Click(View view){
 
-        SubjectLecturer subLec = new SubjectLecturer();
-
-        subLec.setTxtLecturer(txtLecturer.getText().toString());
-        subLec.setTxtEmail(txtEmail.getText().toString());
-        subLec.setTxtPassword(txtPassword.getText().toString());
-        subLec.setTxtConfirmPassword(txtConfirmPassword.getText().toString());
-
-        subLec.setCkbSPDC(ckbSPDC.isEnabled());
-        subLec.setCkbHCI(ckbHCI.isEnabled());
-        subLec.setCkbSE3(ckbSE3.isEnabled());
-        subLec.setCkbSEP(ckbSEP.isEnabled());
-        subLec.setCkbMAD(ckbMAD.isEnabled());
-        subLec.setCkbSE2(ckbSE2.isEnabled());
-        subLec.setCkbITP(ckbITP.isEnabled());
-        subLec.setCkbITA(ckbITA.isEnabled());
-        subLec.setCkbPS(ckbPS.isEnabled());
-        subLec.setCkbSPD(ckbSPD.isEnabled());
-        subLec.setCkbST2(ckbST2.isEnabled());
-        subLec.setCkbST1(ckbST1.isEnabled());
+        String lacturer = txtLecturer.getText().toString();
+        String email = txtEmail.getText().toString();
+        String password = txtPassword.getText().toString();
+        String confirmPassword = txtConfirmPassword.getText().toString();
 
 
-        if(txtPassword.getText().toString().equals(txtConfirmPassword.getText().toString())){
-            requestQueue = Volley.newRequestQueue(this);
-
-            btnNext1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    request = new StringRequest(Request.Method.POST, dashboard_url, new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            try {
-                                JSONObject jsonObject = new JSONObject(response);
-                                if(jsonObject.names().get(0).equals("success")){
-                                    Toast.makeText(getApplicationContext(),"SUCCESS "+jsonObject.getString("success"),Toast.LENGTH_LONG).show();
-                                    callAdminScheduleActivity();
-                                }else{
-                                    Toast.makeText(getApplicationContext(),"Error "+jsonObject.getString("error"),Toast.LENGTH_LONG).show();
-                                }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-
-                        }
-                    }){
-                        @Override
-                        protected Map<String, String> getParams() throws AuthFailureError {
-                            HashMap<String,String> hashMap = new HashMap<String, String>();
-                            hashMap.put("Lecturer",txtLecturer.getText().toString());
-                            hashMap.put("Email",txtEmail.getText().toString());
-                            hashMap.put("Password",txtPassword.getText().toString());
-
-                            return hashMap;
-                        }
-                    };
-                    requestQueue.add(request);
-                }
-            });
-        }else{
-            Toast.makeText(getApplicationContext(),"Password and Confirm Password doesn't match",Toast.LENGTH_LONG).show();
+        if(ckbSPDC.isChecked()){
+            subjectStr+="1@";
         }
+        if(ckbSE3.isChecked()){
+            subjectStr+="2@";
+        }
+        if(ckbHCI.isChecked()){
+            subjectStr+="3@";
+        }
+        if(ckbSEP.isChecked()){
+            subjectStr+="4@";
+        }
+        if(ckbMAD.isChecked()){
+            subjectStr+="5@";
+        }
+        if(ckbSE2.isChecked()){
+            subjectStr+="6@";
+        }
+        if(ckbITP.isChecked()){
+            subjectStr+="7@";
+        }
+        if(ckbITA.isChecked()){
+            subjectStr+="8@";
+        }
+        if(ckbPS.isChecked()){
+            subjectStr+="9@";
+        }
+        if(ckbSPD.isChecked()){
+            subjectStr+="10@";
+        }
+        if(ckbST2.isChecked()){
+            subjectStr+="11@";
+        }
+        if(ckbST1.isChecked()){
+            subjectStr+="12@";
+        }
+        //Toast.makeText(getApplicationContext(),txtLecturer.getText().toString(),Toast.LENGTH_LONG).show();
+
+        if(!lacturer.equals("") && !email.equals("") && !password.equals("") && !confirmPassword.equals("") && !subjectStr.equals("")){
+            if (txtPassword.getText().toString().equals(txtConfirmPassword.getText().toString())){
+
+                Map<String, String>  map = new HashMap<String, String>();
+                map.put("Lecturer",txtLecturer.getText().toString());
+                map.put("Email",txtEmail.getText().toString());
+                map.put("Password",txtPassword.getText().toString());
+                map.put("subjectStr",subjectStr);
+                subjectStr = "";//clear the str
+
+                //RequestQueue mRequestQueue = Volley.newRequestQueue(this);
+                requestQueue = Volley.newRequestQueue(this);
+                // prepare the Request
+                CustomRequest jsObjRequest = new CustomRequest(Request.Method.POST, dashboard_url, map, new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+
+                                try {
+
+                                    String lid = response.getString("lidvalue");
+                                    String[] dayArray = new String[7];
+                                    dayArray[0] = response.getString("mon");dayArray[1] = response.getString("tue");
+                                    dayArray[2] = response.getString("wed");dayArray[3] = response.getString("thu");
+                                    dayArray[4] = response.getString("fri");dayArray[5] = response.getString("sat");
+                                    dayArray[6] = response.getString("sun");
+
+                                    if(response.names().get(0).equals("successes")){
+                                        Toast.makeText(getApplicationContext(),"SUCCESS "+response.getString("successes"),Toast.LENGTH_LONG).show();
+                                        callAdminScheduleActivity(lid,dayArray);
+                                    }else{
+                                        Toast.makeText(getApplicationContext(),"Error "+response.getString("error"),Toast.LENGTH_LONG).show();
+                                    }
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                //Log.d("Error.Response", );
+                            }
+                        });
+
+                // add it to the RequestQueue
+                requestQueue.add(jsObjRequest);
+                //AppController.getInstance().addToRequestQueue(jsObjRequest);
+
+            }else{
+                Toast.makeText(getApplicationContext(),"Password and Confirm Password doesn't match",Toast.LENGTH_LONG).show();
+            }
+        }else{
+            Toast.makeText(getApplicationContext(),"Feilds can not be null",Toast.LENGTH_LONG).show();
+        }
+
 
 
     }
 
-    /*class BackgroundDashboardTask extends AsyncTask<Object,Void,String> {
-        Context context;
-        AlertDialog alertDialog;
-        public BackgroundDashboardTask(Context ctx) {
-            context = ctx;
-        }
 
-        @Override
-        protected void onPreExecute() {
-            //alertDialog = new AlertDialog.Builder(context).create();
-            //alertDialog.setTitle("Login Status");
-
-        }
-
-        @Override
-        protected String doInBackground(Object... params) {
-            String type =(String)params[0];
-            String login_url = "http://quick-appointment.b2creations.net/login.php";
-            if(type.equals("login")){
-                try {
-                    SubjectLecturer subLec = (SubjectLecturer)params[1];
-
-                    URL url = new URL(login_url);
-                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                    httpURLConnection.setRequestMethod("POST");
-                    httpURLConnection.setDoOutput(true);
-                    httpURLConnection.setDoInput(true);
-                    OutputStream outputStream = httpURLConnection.getOutputStream();
-                    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
-
-                    String post_data = URLEncoder.encode("lecturer", "UTF-8")+"="+URLEncoder.encode(subLec.getTxtLecturer(),"UTF-8")+"&"
-                            +URLEncoder.encode("email", "UTF-8")+"="+URLEncoder.encode(subLec.getTxtEmail(), "UTF-8")+"&"
-                            +URLEncoder.encode("password", "UTF-8")+"="+URLEncoder.encode(subLec.getTxtPassword(), "UTF-8")+"&"
-                            +URLEncoder.encode("isSPDC", "UTF-8")+"="+URLEncoder.encode((boolean)subLec.isCkbSPDC(), "UTF-8")+"&"
-                            +URLEncoder.encode("email", "UTF-8")+"="+URLEncoder.encode(subLec.getTxtEmail(), "UTF-8")+"&"
-                            +URLEncoder.encode("email", "UTF-8")+"="+URLEncoder.encode(subLec.getTxtEmail(), "UTF-8");
-
-                    bufferedWriter.write(post_data);
-                    bufferedWriter.flush();
-                    bufferedWriter.close();
-                    outputStream.close();
-                    InputStream inputStream = httpURLConnection.getInputStream();
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
-                    String result = "";
-                    String line = "";
-                    while ((line = bufferedReader.readLine())!= null){
-                        result += line;
-                    }
-                    bufferedReader.close();
-                    inputStream.close();
-                    httpURLConnection.disconnect();
-                    return  result;
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-
-            }
-
-            return null;
-        }
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            LoginRedirect loginRedirect = new LoginRedirect();
-            User user = loginRedirect.getJSON(result);
-
-            if(user.isLogin_status() == true){
-
-                Toast.makeText(context, "Login success", Toast.LENGTH_LONG).show();
-                if(user.getUser_type()== 3) {
-                    Intent intent = new Intent(context, AdminDashboard.class);
-                    context.startActivity(intent);
-                    ((Activity) context).finish();
-
-                }else if(user.getUser_type()== 2) {
-                    Intent intent = new Intent(context, LecturerDashboard.class);
-                    context.startActivity(intent);
-                    ((Activity) context).finish();
-                }
-                else if(user.getUser_type()== 1) {
-                    Intent intent = new Intent(context, StudentDashBoard.class);
-                    context.startActivity(intent);
-                    ((Activity) context).finish();
-                }
-            }else {
-                Toast.makeText(context, result, Toast.LENGTH_LONG).show();
-            }
-
-
-
-            //alertDialog.setMessage(result);
-            //alertDialog.show();
-        }
-    }*/
 
 }
